8aa6e14161707d070f470df05a3854c38ae41cf7
Brad Crysler <bradcrysler@gmail.com>
2018-11-19 10:00:17 -0600

    Stufff

--- develop.py
+++ develop.py
@@ -2,6 +2,8 @@
 
 from app.app_factories import init_runination_app
 
+# Stuff for testing
+
 if __name__ == '__main__':
     app = init_runination_app(debug=True, config_name='qa')
     app.run(host='0.0.0.0', port=8000)
